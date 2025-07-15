1f0fc03f0d319f5accde937dc2543cc810ffb881
Hudson <hbamaster@hotmail.com>
2025-07-15 00:57:11 -0300

    Adiciona frontend ao projeto

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
-import axios from "axios";
+import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "https://precificador-lika.onrender.com/api/fretes"
-});
+  baseURL: import.meta.env.VITE_API_URL
+})
