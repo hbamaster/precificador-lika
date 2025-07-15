ae84771c0831b6ce050f22804e53a349e010d43a
Hudson <hbamaster@hotmail.com>
2025-07-15 01:19:42 -0300

    Corrige entrada do build para Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://precificador-lika.onrender.com',
-        changeOrigin: true,
-        secure: false,
-        rewrite: path => path.replace(/^\/api/, '')
-      }
+  build: {
+    rollupOptions: {
+      input: 'public/index.html'
     }
   }
 })
