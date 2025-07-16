b648ba00df0d2938a6ad0dbb5ce69b6db01e3927
Hudson <hbamaster@hotmail.com>
2025-07-16 00:23:31 -0300

    Limpeza das pastas do backend para reestruturação

--- backend/tests/test_api.py
+++
@@ -1,9 +0,0 @@
-# tests/test_api.py
-from fastapi.testclient import TestClient
-from app.main import app
-
-client = TestClient(app)
-
-def test_healthcheck():
-    res = client.get("/health")
-    assert res.status_code == 200
